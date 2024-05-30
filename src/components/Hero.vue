ab368a05f98f3bb28ebd2cf98f8465fe18427287
Jakub <jakrep01@easv365.dk>
2024-05-30 09:47:37 +0200

    navbar fix

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -65,6 +65,11 @@ onUnmounted(() => {
 }
   
 .statues {
+  position: absolute;
+  margin-left: auto;
+  margin-right: auto;
+  left: 0;
+  right: 0;
   top: 330px;
 }
   
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar fixed w-full bg-dark_purple bg-opacity-70 z-50 font-body text-white text-lg md:px-24 py-4 px-6">
+  <nav class="navbar fixed w-full bg-dark_purple bg-opacity-70 z-50 font-body text-white text-lg py-4 px-6 sm:px-8 lg:px-14 xl:28">
     <a href="#home" class="logo-link">
       <img src="../assets/logo.png" alt="FantasyFestival Logo"/>
     </a>
@@ -10,31 +10,42 @@
       <li class="nav-item"><a @click="toggleMenu" href="#tickets" class="fs-nav nav-link font-semibold">TICKETS</a></li>
       <li class="nav-item"><a @click="toggleMenu" href="#characters" class="fs-nav nav-link font-semibold">CHARACTERS</a></li>
     </ul>
-    <div class="hamburger " :class="{ 'active': isMenuOpen }" @click="toggleMenu">
+    <div class="hamburger" :class="{ 'active': isMenuOpen }" @click="toggleMenu">
       <span :class="{ 'bar': true, 'active': isMenuOpen }" v-for="n in 3" :key="n"></span>
     </div>
-    <a href="#tickets">
-    <button class="bg-yellow hover:bg-amber-400 text-black font-bold font-body px-5 py-2 hidden md:block ml-2">BUY TICKET!</button>
+    <a v-if="isButtonVisible" href="#tickets">
+      <button class="bg-yellow hover:bg-amber-400 text-black font-bold font-body px-5 py-2">BUY TICKET!</button>
     </a>
   </nav>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 
 const isMenuOpen = ref(false);
+const isButtonVisible = ref(window.innerWidth > 1150);
 
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value;
 };
+
+const checkWindowSize = () => {
+  isButtonVisible.value = window.innerWidth > 1150;
+};
+
+onMounted(() => {
+  window.addEventListener('resize', checkWindowSize);
+  checkWindowSize(); // Initial check when component is mounted
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', checkWindowSize);
+});
 </script>
 
 <style lang="scss" scoped>
 /* Navigation */
 button {
-  border: none;
-  cursor: pointer;
-  padding: 0.5rem 1rem;
   transition: background-color 0.3s;
 }
 
@@ -52,11 +63,10 @@ button {
   gap: 2rem;
 }
 
-.nav-link{
+.nav-link {
   transition: 0.3s ease;
   text-decoration: none;
   color: var(--clr-White, #fff);
-
 }
 
 .nav-link:hover {
@@ -68,80 +78,74 @@ button {
   padding: 1em;
 }
 
-
 .hamburger {
   display: none;
 }
 
-@media (max-width: 900px) {
-button{
+@media (max-width: 800px) {
+button {
   display: none;
 }
 
-.navbar{
+.navbar {
   justify-content: space-between;
 }
 
-
-
-
 .navbar img {
   width: auto;
-  height: 64px;
+  height: 56px;
 }
 
-
-  .nav-menu {
-    background-color: #181532;
-    position: absolute;
-    top: 0;
-    left: -100%;
-    width: 100%;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 0;
-    transition: 0.3s;
-  }
-
-  .nav-item {
-    margin: 1em 0;
-  }
-
-  .hamburger {
-    position: relative;
-    display: block;
-    cursor: pointer;
-    z-index: 500;
-    transition: 0.3s;
-  }
-
-  .hamburger.active .bar:nth-child(2) {
-    opacity: 0;
-  }
-  .hamburger.active .bar:nth-child(1) {
-    transform: translateY(8px) rotate(45deg);
-  }
-  .hamburger.active .bar:nth-child(3) {
-    transform: translateY(-8px) rotate(-45deg);
-  }
-
-  .bar {
-    z-index: 550;
-    display: block;
-    width: 25px;
-    height: 3px;
-    margin: 5px auto;
-    -webkit-transition: all 0.3s ease-in-out;
-    transition: all 0.3s ease-in-out;
-    background-color: var(--clr-White, #fff);
-  }
+.nav-menu {
+  background-color: #181532;
+  position: absolute;
+  top: 0;
+  left: -100%;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0;
+  transition: 0.3s;
 }
 
-@media (max-width: 1200px) {
-  button{
-    display: none;
-  }
+.nav-item {
+  margin: 1em 0;
+}
+
+.hamburger {
+  position: relative;
+  display: block;
+  cursor: pointer;
+  z-index: 500;
+  transition: 0.3s;
+}
+
+.hamburger.active .bar:nth-child(2) {
+  opacity: 0;
+}
+.hamburger.active .bar:nth-child(1) {
+  transform: translateY(8px) rotate(45deg);
+}
+.hamburger.active .bar:nth-child(3) {
+  transform: translateY(-8px) rotate(-45deg);
+}
+
+.bar {
+  z-index: 550;
+  display: block;
+  width: 25px;
+  height: 3px;
+  margin: 5px auto;
+  -webkit-transition: all 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out;
+  background-color: var(--clr-White, #fff);
+}
+}
+
+@media (max-width: 1150px) {
+button {
+  display: none;
+}
 }
 </style>
-
